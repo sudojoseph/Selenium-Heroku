c831a08c3a995694213178334eb1708ba03fab91
Joseph Davidson <joseph@yaballe.com>
2024-04-07 10:38:37 +0200

    fix

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from selenium import webdriver
 import os
 
 chrome_options = webdriver.ChromeOptions()
-chrome_options.add_argument("--headless", "--disable-gpu", "--no-sandbox")
+chrome_options.add_argument("--headless")
 # chrome_options.add_argument("--disable-dev-shm-usage")
 # chrome_options.add_argument("--no-sandbox")
 
